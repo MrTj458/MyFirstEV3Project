7bac841d60f927c9efc235b0bb712a4b3da19f08
Trevor <thod0127@740s-pl-130.local>
2015-12-10 13:55:38 -0700

    Added components to the controller

--- src/robot/controller/BotController.java
+++ src/robot/controller/BotController.java
@@ -1,14 +1,36 @@
 package robot.controller;
 
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+import robot.model.EV3Bot;
+
 public class BotController
 {
+	private String message;
+	
+	private int xPosition;
+	private int yPosition;
+	
+	private long waitTime;
+	
+	private EV3Bot tjBot;
+	
 	public BotController()
 	{
+		this.message = "Legos with Lejos!";
+		this.xPosition = 10;
+		this.yPosition = 10;
+		this.waitTime = 4000;
 		
+		tjBot = new EV3Bot();
 	}
 	
 	public void start()
 	{
+		LCD.drawString(message, xPosition, yPosition);
+		Delay.msDelay(waitTime);
 		
+		tjBot.driveRoom();
 	}
 }
