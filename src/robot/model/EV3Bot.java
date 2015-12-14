f640a2888fe0a9fe046a2aea25a073702499c92a
Trevor <thod0127@740s-pl-130.local>
2015-12-14 12:46:36 -0700

    Added contents to EV3Bot.java

--- src/robot/model/EV3Bot.java
+++ src/robot/model/EV3Bot.java
@@ -1,6 +1,56 @@
 package robot.model;
 
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.Motor;
+import lejos.utility.Delay;
+
 public class EV3Bot
 {
-
+	private String botMessage;
+	private int xPosition;
+	private int yPosition;
+	private long waitTime;
+	
+	public EV3Bot()
+	{
+		this.botMessage = "Lejos!";
+		this.xPosition = 50;
+		this.yPosition = 50;
+		this.waitTime = 4000;
+	}
+	
+	public void driveRoom()
+	{
+		displayMessage();
+		circles();
+		drive();
+	}
+	
+	private void displayMessage()
+	{
+		LCD.drawString(botMessage, xPosition, yPosition);
+		Delay.msDelay(waitTime);
+	}
+	
+	private void displayMessage(String message)
+	{
+		LCD.drawString(message, xPosition, yPosition);
+		Delay.msDelay(waitTime);
+	}
+	
+	private void circles()
+	{
+		displayMessage("Circles!");
+		Motor.A.setSpeed(720);
+		Motor.B.setSpeed(-720);
+		Delay.msDelay(waitTime);
+	}
+	
+	private void drive()
+	{
+		displayMessage("Driving forward!");
+		Motor.A.forward();
+		Motor.B.forward();
+		Delay.msDelay(waitTime);
+	}
 }
